c149b8b2bae060b6dacfab4fd90eb25effbe9bd1
Rodrigo Bossini <professorbossini@gmail.com>
2024-08-06 18:07:15 -0300

    js: encerra o codigo 1.5.1
    - mostra exemplos basicos de vetores
    - retomar falando de funcoes que operam sobre vetores

---
+++ javascript/index.js
@@ -0,0 +1,67 @@
+const v = [1, 2, 3]
+console.log(v)
+// v.push(4)
+v = [1, 2, 3]
+console.log(v)
+
+// v1 = []
+// console.log(v1.length)
+// v1[0] = 3.4
+// console.log(v1.length)
+// v1[10] = "abc"
+// console.log(v1.length)
+// v2 = [2, "abc", true]
+// console.log(v2)
+// for (let i = 0; i < v2.length; i++)
+//   console.log(v2[i])
+// //comparação == e ===
+// console.log(1 == 1)
+// console.log(1 == "1")
+// console.log(1 === 1)
+// console.log(1 === "1")
+// console.log(true == 1)
+// //[1]
+// console.log (1 == [1])
+// // const j = undefined
+// console.log(null == null)
+// console.log(null == undefined)
+// console.log([] == false)
+// console.log([] == [])
+// const n1 = 2
+// const n2 = '3'
+// //coerção implícita
+// const n3 = n1 + n2
+// console.log(n3)
+// //coerção explícita
+// const n4 = n1 + Number(n2)
+// console.log(n4)
+
+// //hoist: içamento
+// //mecanismo de dupla passagem do js
+// var idade = 18
+// console.log(`Oi, ${nome}`)
+// if(idade >= 18){
+//   let nome = "João"
+//   console.log(`Parabéns, ${nome}. Você pode dirigir`)
+// }
+// console.log("Até, " + nome)
+
+
+// var linguagem = "Javascript"
+// console.log("Aprendendo " + linguagem)
+// var linguagem = "Java"
+// console.log("Aprendendo " + linguagem)
+
+// //const, let e var
+// //use const para aplicar o princípio do menor privilégio
+// const nome = "Luiz"
+// console.log(nome)
+// // nome = "João"
+// let a = 2
+// console.log(a)
+// console.log(typeof(a))
+// a = "João"
+// console.log(a)
+// console.log(typeof(a))
+// var b = "oi"
+// b = "tchau"
