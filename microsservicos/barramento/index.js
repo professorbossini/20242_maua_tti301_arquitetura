309191fbc69f93abfed8969b02f87603a5d723d6
Rodrigo Bossini <professorbossini@gmail.com>
2024-09-11 11:13:54 -0300

    t1(mss): encerra a secao 4.3.38
    - retomar fazendo o teste do mss de consulta na secao 4.3.39

--- microsservicos/barramento/index.js
+++ microsservicos/barramento/index.js
@@ -10,14 +10,11 @@ const { PORT } = process.env
 //definir o endpoint
 //POST /eventos
 app.post('/eventos', (req, res) => {
-  //1. pega o corpo da requisição
   const evento = req.body 
   console.log(evento) 
-  //2. envia para o mss de lembretes na porta 4000 (post /eventos)
   axios.post('http://localhost:4000/eventos', evento)
-  //3. envia para o mss de observações na porta 5000 (post /eventos)
   axios.post('http://localhost:5000/eventos', evento)
-  //4. encerra com código 200, devolvendo uma eventual mensagem de sucesso
+  axios.post('http://localhost:6000/eventos', evento)
   res.status(200).json({mensagem: 'ok'})
 })
 
--- microsservicos/consulta/index.js
+++ microsservicos/consulta/index.js
@@ -5,10 +5,31 @@ app.use(express.json())
 
 const { PORT } = process.env
 
-//GET /lembretes
+const baseConsulta = {}
 
+const funcoes = {
+  LembreteCriado: (lembrete) => {
+    baseConsulta[lembrete.id] = lembrete
+  },
+  ObservacaoCriada: (observacao) => {
+    const observacoes = baseConsulta[observacao.lembreteId]['observacoes'] || []
+    observacoes.push(observacao)
+    baseConsulta[observacao.lembreteId]['observacoes'] = observacoes
+  },
+  
+}
+
+
+//GET /lembretes
+app.get('/lembretes', (req, res) => {
+  res.json(baseConsulta)
+})
 
 //POST /eventos
+app.post('/eventos', (req, res) => {
+  const evento = req.body
+  funcoes[evento.type](evento.payload)  
+})
 
 
 app.listen(PORT, () => console.log(`Consulta. ${PORT}`))
