4aa68fffb94baedb238ae716d192abb499cf0643
asbonato <asbonato@gmail.com>
2024-10-01 20:50:32 -0300

    t3(docker): Hey_Docker e comando cli finalizados     - retomar 4.3.58 - implantando mss lembretes

---
+++ hello_docker/index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+app.use(express.json())
+
+app.get('/hey_docker', (req, res) => {
+    res.status(200).json({
+        mensagem: "Hey, Docker!!"
+    })
+})
+app.listen(5200, () => console.log("up and running inside docker"))
